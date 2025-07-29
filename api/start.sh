6d2148de5bc6a3b31cfc94cdf9d775b900ceb3e3
rohan908 <145878828+rohan908@users.noreply.github.com>
2025-07-29 18:22:06 -0400

    pls work

--- api/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Get port from environment variable, default to 8000
-PORT=${PORT:-8000}
-
-echo "Starting FastAPI application on port $PORT"
-
-# Start the application
-exec uvicorn app.main:app --host 0.0.0.0 --port $PORT 
